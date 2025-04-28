becdfa0903d079d3c75a46bc96aaf2f121cc8d90
Adam Kamaleddine <akamaleddine82@Adams-MacBook-Pro-4.local>
2025-04-27 19:17:35 -0500

    Fixed Lab 12 with corrected Television class and tests

--- television.py
+++ television.py
@@ -1,46 +1,83 @@
+# television.py
+
+"""
+Television Class
+"""
+
 class Television:
+    """
+    A class representing a Television.
+    """
+
+    
     MIN_VOLUME = 0
-    MAX_VOLUME = 2
+    MAX_VOLUME = 10
     MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MAX_CHANNEL = 10
 
-    def __init__(self):
-        self._status = False
-        self._muted = False
-        self._volume = Television.MIN_VOLUME
-        self._channel = Television.MIN_CHANNEL
+    def __init__(self) -> None:
+        """
+        Initialize the Television with default values.
+        """
+        self.__status = False
+        self.__muted = False
+        self.__volume = 5
+        self.__channel = 0
 
-    def turn_on(self):
-        self._status = True
+    def power(self) -> None:
+        """
+        Turn the TV on or off.
+        """
+        self.__status = not self.__status
 
-    def turn_off(self):
-        self._status = False
+    def mute(self) -> None:
+        """
+        Mute or unmute the TV.
+        """
+        if self.__status:
+            self.__muted = not self.__muted
 
-    def mute(self):
-        if self._status:
-            self._muted = not self._muted
+    def channel_up(self) -> None:
+        """
+        Increase the channel by 1. Wrap around if at max channel.
+        """
+        if self.__status:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
 
-    def channel_up(self):
-        if self._status:
-            self._channel += 1
-            if self._channel > Television.MAX_CHANNEL:
-                self._channel = Television.MIN_CHANNEL
+    def channel_down(self) -> None:
+        """
+        Decrease the channel by 1. Wrap around if at min channel.
+        """
+        if self.__status:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
 
-    def channel_down(self):
-        if self._status:
-            self._channel -= 1
-            if self._channel < Television.MIN_CHANNEL:
-                self._channel = Television.MAX_CHANNEL
+    def volume_up(self) -> None:
+        """
+        Increase the volume by 1 unless muted or at max volume.
+        """
+        if self.__status and not self.__muted:
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
 
-    def volume_up(self):
-        if self._status and not self._muted:
-            if self._volume < Television.MAX_VOLUME:
-                self._volume += 1
+    def volume_down(self) -> None:
+        """
+        Decrease the volume by 1 unless muted or at min volume.
+        """
+        if self.__status and not self.__muted:
+            if self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
 
-    def volume_down(self):
-        if self._status and not self._muted:
-            if self._volume > Television.MIN_VOLUME:
-                self._volume -= 1
+    def __str__(self) -> str:
+        """
+        Return a string representation of the TV status.
+        """
+        return (f"Power = {self.__status}, "
+                f"Channel = {self.__channel}, "
+                f"Volume = {self.__volume if not self.__muted else 0}")
 
-    def __str__(self):
-        return f"TV status: {'On' if self._status else 'Off'}, Channel: {self._channel}, Volume: {self._volume}, Muted: {self._muted}"
--- test_tv.py
+++ test_tv.py
@@ -1,36 +1,60 @@
+# test_tv.py
+
+import pytest
 from television import Television
 
-def test_power_toggle():
+def test_initial_state():
     tv = Television()
-    assert not tv._status
-    tv.turn_on()
-    assert tv._status
-    tv.turn_off()
-    assert not tv._status
+    assert tv._Television__status == False
+    assert tv._Television__muted == False
+    assert tv._Television__volume == 5
+    assert tv._Television__channel == 0
 
-def test_channel_wraparound_up():
+def test_power():
     tv = Television()
-    tv.turn_on()
-    tv._channel = Television.MAX_CHANNEL
-    tv.channel_up()
-    assert tv._channel == Television.MIN_CHANNEL
+    tv.power()
+    assert tv._Television__status == True
+    tv.power()
+    assert tv._Television__status == False
 
-def test_volume_blocked_when_muted():
+def test_mute():
     tv = Television()
-    tv.turn_on()
-    tv.volume_up()
+    tv.power()
     tv.mute()
-    tv.volume_up()
-    assert tv._volume == 1  
+    assert tv._Television__muted == True
+    tv.mute()
+    assert tv._Television__muted == False
+
+def test_channel_up_wrap():
+    tv = Television()
+    tv.power()
+    tv._Television__channel = Television.MAX_CHANNEL
+    tv.channel_up()
+    assert tv._Television__channel == Television.MIN_CHANNEL
 
 def test_channel_down_wrap():
     tv = Television()
-    tv.turn_on()
-    tv._channel = Television.MIN_CHANNEL
+    tv.power()
+    tv._Television__channel = Television.MIN_CHANNEL
     tv.channel_down()
-    assert tv._channel == Television.MAX_CHANNEL
+    assert tv._Television__channel == Television.MAX_CHANNEL
 
-def test_str_output():
+def test_volume_up_max():
     tv = Television()
-    expected = "TV status: Off, Channel: 0, Volume: 0, Muted: False"
-    assert str(tv) == expected
+    tv.power()
+    tv._Television__volume = Television.MAX_VOLUME
+    tv.volume_up()
+    assert tv._Television__volume == Television.MAX_VOLUME
+
+def test_volume_down_min():
+    tv = Television()
+    tv.power()
+    tv._Television__volume = Television.MIN_VOLUME
+    tv.volume_down()
+    assert tv._Television__volume == Television.MIN_VOLUME
+
+def test_str_muted_volume():
+    tv = Television()
+    tv.power()
+    tv.mute()
+    assert "Volume = 0" in str(tv)
